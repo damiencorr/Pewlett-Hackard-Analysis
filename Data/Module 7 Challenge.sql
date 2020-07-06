031f098ede05fccb738968e9bdc45ae503c9b295
damiencorr <damien.corr@gmail.com>
2020-07-05 23:28:07 -0400

    Credit for Weekly Lessons:

--- Data/Module 7 Challenge.sql
+++ Data/Module 7 Challenge.sql
@@ -204,3 +204,18 @@ WHERE d.dept_name in ('Sales','Development')
 ;
 
 
+
+
+
+
+
+
+
+-- Module 7 Challenge
+-- Submit the following deliverables:
+-- 1. Delivering Results: A README.md in the form of a technical report that details your analysis and findings
+-- 2. Technical Analysis Deliverable 1: Number of Retiring Employees by Title. You will create three new tables, one showing number of [titles] retiring, one showing number of employees with each title, and one showing a list of current employees born between Jan. 1, 1952 and Dec. 31, 1955. New tables are exported as CSVs. 
+-- 3. Technical Analysis Deliverable 2: Mentorship Eligibility. A table containing employees who are eligible for the mentorship program You will submit your table and the CSV containing the data (and the CSV containing the data)
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Pewlett-Hackard-Analysis
+
+## Module7
+
+## Challenge
+
+### Contents
+
+Technical report consists of three paragraphs:
+
+1. Introduce the problem that you were using data to solve.
+2. Summarize the steps that you took to solve the problem, as well as the challenges that you encountered along the way.
+    - provide examples and descriptions of the code that you used.
+3. Results of analysis and discuss the generated data
+    - Any limitations to the analysis?
+    - What next steps would you recommend?
+
+- Image of the ERD mapping out the database
+
+## Deliverable 1: Number of Retiring Employees by Title
+
+
+
+## Deliverable 2: Mentorship Eligibility
+
+
+
+## Putting It All Together
+
+
